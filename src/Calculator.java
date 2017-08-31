b97f89abfce534904be7778c91651bb5a3a312da
GavinDes <gavin17.business@gmail.com>
2017-08-30 22:04:17 -0500

    Changed floats into doubles and added loop.

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,56 +10,58 @@ import java.util.Scanner;
 public class Calculator 
 {
 
-    public static float add(float value1, float value2)
+    public static double add(double value1, double value2)
     {
         return value1 + value2;
     }
     
-    public static float subtract(float value1, float value2)
+    public static double subtract(double value1, double value2)
     {
         return value1 - value2;
     }
     
-    public static float multiply(float value1, float value2)
+    public static double multiply(double value1, double value2)
     {
         return value1 * value2;
     }
     
-    public static float divide(float value1, float value2)
+    public static double divide(double value1, double value2)
     {
         return value1 / value2;
     }
     public static void main(String[] args) 
     {
-        System.out.print("opperation\n>: ");
-        Scanner input = new Scanner(System.in);
-        String opperation = input.nextLine();
-        System.out.print("value1\n>: ");
-        Float value1 = input.nextFloat();
-        System.out.print("value2\n>: ");
-        Float value2 = input.nextFloat();
-        if(opperation.equals("add")){
-            float solution = add(value1, value2);
-            System.out.println(solution);
-        }else if(opperation.equals("subtract")){
-            float solution = subtract(value1, value2);
-            System.out.println(solution);
-        }else if(opperation.equals("multiply")){
-            float solution = multiply(value1, value2);
-            System.out.println(solution);
-        }else if(opperation.equals("divide")){
-            float solution = divide(value1, value2);
-            System.out.println(solution);
-        }else{
-            // TODO: Put continue for loop
+        while(true){
+            System.out.print("\nopperation\n>: ");
+            Scanner input = new Scanner(System.in);
+            String opperation = input.nextLine();
+            System.out.print("value1\n>: ");
+            double value1 = input.nextDouble();
+            System.out.print("value2\n>: ");
+            double value2 = input.nextDouble();
+            if(opperation.equals("add")){
+                double solution = add(value1, value2);
+                System.out.println(solution);
+            }else if(opperation.equals("subtract")){
+                double solution = subtract(value1, value2);
+                System.out.println(solution);
+            }else if(opperation.equals("multiply")){
+                double solution = multiply(value1, value2);
+                System.out.println(solution);
+            }else if(opperation.equals("divide")){
+                double solution = divide(value1, value2);
+                System.out.println(solution);
+            }else{
+                System.out.printf("no %s opperation\n", opperation);
+                continue;
+            }
+            break;
         }
     }
     
 }
 
 /*
-TODO: loop around
 TODO: turn if statements into switch
-TODO: make switch/if into its own method
-TODO: use double rather than float
+TODO: make switch into its own method
 */
